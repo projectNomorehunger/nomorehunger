f70d524ebf30f7b52f503e64d1a59968ec64df35
projectNomorehunger <nomorehungerproject807@gmail.com>
2024-03-09 15:26:54 +0700

    -add dmgbuffer map4

---
+++ nomorehunger/Assets/PlayerStatsChange.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatsChange : MonoBehaviour
+{   
+    public bool isBuff;
+    private void Start()
+    {
+        isBuff = false;
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && !isBuff)
+        {
+            PlayerStats.instance.BuffDamage();
+            isBuff = true;
+            //Debug.Log("isbuff");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+
+        }
+    }
+
+
+}
